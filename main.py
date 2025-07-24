613405ff8e27c31e8333aa5e100fab85ddd31b7c
MichelRosselli <michelrosselli@gmail.com>
2025-07-24 12:04:42 +0200

    Fix import statement for HyperAgent in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 from argparse import ArgumentParser
 
-from src.hyperagent import HyperAgent
+from hyperagent import HyperAgent
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
